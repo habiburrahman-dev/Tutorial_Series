aca74e9bb5cc4e66121f9ec4d0d79f09d658643d
[Habiburrahman] <[habiburrahman.dev@gmail.com]>
2022-01-20 16:05:19 +0700

    Penambahan Dialog

--- app/src/main/java/dev/habiburrahman/tutorialseries/utils/Constant.kt
+++ app/src/main/java/dev/habiburrahman/tutorialseries/utils/Constant.kt
@@ -3,4 +3,7 @@ package dev.habiburrahman.tutorialseries.utils
 object Constant {
     const val FINISHED = "activity_finished"
     const val CONTRACT_DATA = "contract_data"
+    const val CUSTOM_DIALOG_1 = "custom_dialog_1"
+    const val CUSTOM_DIALOG_2 = "custom_dialog_2"
+    const val CUSTOM_DIALOG_3 = "custom_dialog_3"
 }
